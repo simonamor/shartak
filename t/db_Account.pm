bcfe5323baf40e67dfdbf2b29bd9d408b853b922
Simon Amor <simon@leaky.org>
2016-11-23 21:35:51 +0000

    A simple test script for the Account module

---
+++ t/db_Account.pm
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::DBIx::Class;
+
+ok my $account = Account->create({
+    email => 'first@example.com',
+    password => 'nothing',
+}), 'Created a test account';
+
+is_result $account;
+
+is_fields [qw(email)], $account,
+    ['first@example.com'],
+    'account record looks good';
+
+isnt $account->password, 'nothing', 'password is not in plain text, good';
+
+done_testing();
