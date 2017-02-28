fb1f100cc0a2eaa1d7d840ba9e527f6093d54527
Simon Amor <simon@leaky.org>
2017-02-28 21:54:08 +0000

    Generate correct urls for uri_for()

--- lib/RPGCat.pm
+++ lib/RPGCat.pm
@@ -50,6 +50,9 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
+
+#    default_view => 'HTML',
+    using_frontend_proxy => 1,
 );
 
 __PACKAGE__->config(
