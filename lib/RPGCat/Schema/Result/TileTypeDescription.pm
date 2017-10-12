c115dbe4886aab36befda5981c37a865c114136f
Simon Amor <simon@leaky.org>
2017-10-12 21:38:57 +0100

    Reworking Tile/Description tables (part 2 - fix names)
    Had to do this in two parts otherwise git wanted to say add+remove instead of rename.

--- lib/RPGCat/Schema/Result/TileTypeDescription.pm
+++ lib/RPGCat/Schema/Result/TileTypeDescription.pm
@@ -1,5 +1,5 @@
 use utf8;
-package RPGCat::Schema::Result::Description;
+package RPGCat::Schema::Result::TileTypeDescription;
 
 use strict;
 use warnings;
@@ -9,24 +9,23 @@ use MooseX::NonMoose;
 use MooseX::MarkAsMethods autoclean => 1;
 extends 'DBIx::Class::Core';
 
-__PACKAGE__->table("descriptions");
+__PACKAGE__->table("tile_type_descriptions");
 
 __PACKAGE__->add_columns(
-  "tile_id",
-  {data_type => "integer",extra => { unsigned => 1 }, is_auto_increment => 1, is_nullable => 0},
-  "tile_description",
-  { data_type => "mediumtext", is_nullable => 0},
-  );
+    "tile_type_id",
+    {data_type => "integer",extra => { unsigned => 1 }, is_auto_increment => 1, is_nullable => 0},
+    "description",
+    { data_type => "mediumtext", is_nullable => 0},
+);
 
 
 
-__PACKAGE__->set_primary_key("tile_id");
+__PACKAGE__->set_primary_key("tile_type_id");
 
 
-__PACKAGE__->belongs_to( 'tile' => 'RPGCat::Schema::Result::Tile', 'tile_id' );
+__PACKAGE__->belongs_to( 'tile_type' => 'RPGCat::Schema::Result::TileType', 'tile_type_id' );
 
 
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
 1;
